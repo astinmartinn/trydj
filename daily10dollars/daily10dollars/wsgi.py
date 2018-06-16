a1b1bd9b29483687199df7052b7f8bca07c6849d
austinmartinn <mugeshkumaradwords@gmail.com>
2018-06-16 13:03:25 +0530

    edited server file

--- daily10dollars/daily10dollars/wsgi.py
+++ daily10dollars/daily10dollars/wsgi.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "daily10dollars.settings")
 
 application = get_wsgi_application()
-from whitenoide.django import DjangoWhiteNoise
 application=DjangoWhiteNoise(application)
